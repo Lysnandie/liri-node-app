49543bc8bf8912a8736dcd9aefebc7362f2785a5
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2017-11-28 23:02:55 -0600

    packge.json

---
+++ keys.js
@@ -0,0 +1,11 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'rm4AuU64bPjxwMtLr5x9R00rG',
+  consumer_secret: 'W76kuoMfyquChFxRyTY68tp42gnxqpcH3ahiGExqZhySUdbuAP'
+,
+  access_token_key: '935277178018779136-65scyeZ8MUWNyFxjvmvZ7WMY8gzND3Q',
+  access_token_secret: 'm6HWtSDpPyzFAFkJmrQzWOn5aTJ9L30B7CEzINWXap1nP',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,10 @@
+// Grabs data from keys.js
+
+var keys = require('./keys.js');
+
+//node package to read and write file
+var fs = require("fs");
+//needed variables
+var request = require('request');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
