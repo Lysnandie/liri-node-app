c661df1e9ce456c3b68d1d2e9aa0b8e1d40a242a
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2017-11-30 18:04:22 -0600

    update

--- liri.js
+++ liri.js
@@ -6,9 +6,9 @@ var fs = require("fs");
 //needed variables
 var request = require('request');
 var Twitter = require('twitter');
-var Spotify = require('spotify-api');
+var Spotify = require('node-spotify-api');
 
-//---------------------------Twitter---------------------------------//
+//---------------------------Twitter------------------------------------------------------//
 var getTweets = function() {
 
 
@@ -21,75 +21,70 @@ var getTweets = function() {
   //nmp function for getTweets
 
   client.get('statuses/user_timeline', params, function(error, tweets, response) {
-      if (!error) {
-        for (var i = 0; i < tweets.length; i++) {
-          var date = tweets[i].created_at;
-          console.log(date, tweets[i].text);
-
-        }
-        //pushes tweets into an emplty array
-        // var myTweets = [];
-        // //loop through array with for loop
-        // for (var i = 0; i < myTweets.length; i++); {
-        // //push myTweets into empty array
-        // //myTweets.push ({
-        // console.log(myTweets);
-        // //})
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        var date = tweets[i].created_at;
+        console.log(date, tweets[i].text);
 
       }
-    });
 
-  };
+    }
+  });
+
+};
 
 getTweets();
 
 
-//============Spotify====================================
+//========================================Spotify==========================================
+var getSpotify = function() {
 //spotify client keys
-// var spotify = new Spotify({
-//   id: '9d8c4ec731dd49df9efffc1876380f57',
-//   secret: 'ad54f251d5674a8583a757dcac346efa'
-// });
-// //npm package search function
-// spotify.search({
-//   type: 'track',
-//   query: 'All the Small Things'
-// }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
-//
-//   console.log(data);
-//
-// });
-// }
+var spotify = new Spotify({
+  id: '9d8c4ec731dd49df9efffc1876380f57',
+  secret: 'ad54f251d5674a8583a757dcac346efa'
+});
+//npm package search function
+spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+
+console.log(data);
+
+});
+}
+
+getSpotify();
 
 
-// //===================OMDB Movie Request========================
-//
-// var movieName = process.argv[2];
-//
-// // Then run a request to the OMDB API with the movie specified
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-//
-//
-//
-// request(queryUrl, function(error, response, body) {
-//
-//   // If the request is successful
-//   if (!error && response.statusCode === 200) {
-//
-//     //parses response on console
-//     console.log(JSON.parse(body));
-//     console.log("Title: " + JSON.parse(body).title);
-//     console.log("Release Year: " + JSON.parse(body).Year);
-//     console.log("Rating: " + JSON.parse(body).imdbRating);
-//     console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings);
-//     console.log("Country of Production: " + JSON.parse(body).Country);
-//     console.log("Language: " + JSON.parse(body).Language);
-//     console.log("Plot: " + JSON.parse(body).Plot);
-//     console.log("Actors: " + JSON.parse(body).Actors);
-//
-//
-//   }
-// });
+//===================OMDB Movie Request======================================================
+
+var getMovie = function(){
+var movieName = process.argv[2];
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    //parses response on console
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
+    console.log("Country of Production: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+
+
+  }
+});
+}
+
+getMovie();
