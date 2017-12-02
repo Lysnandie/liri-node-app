158251fe669cd16478178a130f834c4bc7558858
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2017-12-01 19:35:20 -0600

    all npm calls working

--- liri.js
+++ liri.js
@@ -7,7 +7,7 @@ var fs = require("fs");
 var request = require('request');
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
-
+var argTwo = process.argv[3];
 
 // SWITCH STATEMENT FOR RUNNING DIFFERENT APPS
 // THIS WILL DIRECT WHICH FUNCTION TO RUN
@@ -65,57 +65,75 @@ var Spotify = require('node-spotify-api');
 //
 // };
 // //run function
-// getTweets();
-
-//========================================Spotify==========================================
-// var getSpotify = function() {
+// // getTweets();
+//
+// //========================================Spotify==========================================
+// var getSpotify = function(){
 // //spotify client keys
 // var spotify = new Spotify({
 //   id: '9d8c4ec731dd49df9efffc1876380f57',
 //   secret: 'ad54f251d5674a8583a757dcac346efa'
 // });
-// //npm package search function
-// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//
+// var song = process.argv[2];
+//
+// //if user doesn't input a song, search "what's my age again" in the search
+// if (song === undefined) {
+//    song = 'What\'s my age again';
+//  };
+//
+// //spotify npm search
+// spotify.search({ type: 'track', query: song }, function(err, data) {
 //   if (err) {
 //     return console.log('Error occurred: ' + err);
 //   }
 //
-// console.log(data);
+// console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
+// console.log("Song Name: " + data.tracks.items[0].name);
+// console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+// console.log("Album: " + data.tracks.items[0].album.name);
+//
+// //append to log txt file
+//  fs.appendFile('log.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n" + "Album: " +
+//  data.tracks.items[0].album.name + "\n");
 //
 // });
 // }
 //
 // getSpotify();
 //
-//
+
 // //===================OMDB Movie Request======================================================
 //
-// var getMovie = function(){
-// var movieName = process.argv[2];
+var getMovie = function(){
+var movieName = process.argv[2];
+
+// Then run a request to the OMDB API with the movie specified
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    //parses response on console
+      var movie = JSON.parse(body);
+    console.log("Title: " + JSON.parse(body).Title);
+    console.log("Release Year: " + JSON.parse(body).Year);
+    console.log("Rating: " + JSON.parse(body).imdbRating);
+    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
+    console.log("Country of Production: " + JSON.parse(body).Country);
+    console.log("Language: " + JSON.parse(body).Language);
+    console.log("Plot: " + JSON.parse(body).Plot);
+    console.log("Actors: " + JSON.parse(body).Actors);
+      fs.appendFile('log.txt', "Title: " + movie.Title + "\n" + "Year: " + movie.Year + "\n" + "IMDB Rating: " + movie.imdbRating + "\n" + "Country: " + movie.Country + "\n" + "Language: " + movie.Language + "\n" +
+      "Plot: " + movie.Plot + "\n" + "Actors: " + movie.Actors + "\n" + "Rotten Tomatoes Rating: " + movie.Ratings[0].Value);
 //
-// // Then run a request to the OMDB API with the movie specified
-// var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
 //
-//
-//
-// request(queryUrl, function(error, response, body) {
-//
-//   // If the request is successful
-//   if (!error && response.statusCode === 200) {
-//
-//     //parses response on console
-//     console.log("Title: " + JSON.parse(body).Title);
-//     console.log("Release Year: " + JSON.parse(body).Year);
-//     console.log("Rating: " + JSON.parse(body).imdbRating);
-//     console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
-//     console.log("Country of Production: " + JSON.parse(body).Country);
-//     console.log("Language: " + JSON.parse(body).Language);
-//     console.log("Plot: " + JSON.parse(body).Plot);
-//     console.log("Actors: " + JSON.parse(body).Actors);
-//
-//
-//   }
-// });
-// }
-//
-// getMovie();
+  }
+});
+}
+
+getMovie();
