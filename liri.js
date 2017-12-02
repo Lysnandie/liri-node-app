8b0dc907ae4fd98120f09ecffb199300e55134a3
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2017-12-01 21:42:33 -0600

    final

--- liri.js
+++ liri.js
@@ -35,8 +35,6 @@ var getTweets = function() {
           'Tweets: ': tweets[i].text,
         });
         console.log(data);
-
-
       }
 
     } else {
@@ -49,73 +47,73 @@ var getTweets = function() {
 getTweets();
 // //
 // // //========================================Spotify==========================================
-// // var getSpotify = function(){
-// // //spotify client keys
-// // var spotify = new Spotify({
-// //   id: '9d8c4ec731dd49df9efffc1876380f57',
-// //   secret: 'ad54f251d5674a8583a757dcac346efa'
-// // });
-// //
-// //
-// // //if user doesn't input a song, search "what's my age again" in the search
-// // if (argTwo === undefined) {
-// //    argTwo = 'What\'s my age again';
-// //  };
-// //
-// // //spotify npm search
-// // spotify.search({ type: 'track', query: argTwo }, function(err, data) {
-// //   if (err) {
-// //     return console.log('Error occurred: ' + err);
-// //   }
-// //
-// // console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
-// // console.log("Song Name: " + data.tracks.items[0].name);
-// // console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
-// // console.log("Album: " + data.tracks.items[0].album.name);
-// //
-// // //append to log txt file
-// //  fs.appendFile('log.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n" + "Album: " +
-// //  data.tracks.items[0].album.name + "\n");
-// //
-// // });
-// // }
-// //
-// // getSpotify();
-// //
+var getSpotify = function(){
+//spotify client keys
+var spotify = new Spotify({
+  id: '9d8c4ec731dd49df9efffc1876380f57',
+  secret: 'ad54f251d5674a8583a757dcac346efa'
+});
+
+
+//if user doesn't input a song, search "what's my age again" in the search
+if (argTwo === undefined) {
+   argTwo = 'What\'s my age again';
+ };
+
+//spotify npm search
+spotify.search({ type: 'track', query: argTwo }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+
+console.log("Artist Name: " + data.tracks.items[0].artists[0].name);
+console.log("Song Name: " + data.tracks.items[0].name);
+console.log("Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify);
+console.log("Album: " + data.tracks.items[0].album.name);
+
+//append to log txt file
+ fs.appendFile('log.txt', "Artist: " + data.tracks.items[0].artists[0].name + "\n" + "Song Name: " + data.tracks.items[0].name + "\n" + "Spotify Preview Link: " + data.tracks.items[0].external_urls.spotify + "\n" + "Album: " +
+ data.tracks.items[0].album.name + "\n" + "\n");
+
+});
+}
+
+getSpotify();
+
 //
 // // //===================OMDB Movie Request======================================================
 // //
-// var getMovie = function() {
-//
-//   // Then run a request to the OMDB API with the movie specified
-//   var queryUrl = "http://www.omdbapi.com/?t=" + argTwo + "&y=&plot=short&apikey=40e9cece";
-//
-//
-//
-//   request(queryUrl, function(error, response, body) {
-//
-//     // If the request is successful
-//     if (!error && response.statusCode === 200) {
-//
-//       //parses response on console
-//       var movie = JSON.parse(body);
-//       console.log("Title: " + JSON.parse(body).Title);
-//       console.log("Release Year: " + JSON.parse(body).Year);
-//       console.log("Rating: " + JSON.parse(body).imdbRating);
-//       console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
-//       console.log("Country of Production: " + JSON.parse(body).Country);
-//       console.log("Language: " + JSON.parse(body).Language);
-//       console.log("Plot: " + JSON.parse(body).Plot);
-//       console.log("Actors: " + JSON.parse(body).Actors);
-//       fs.appendFile('log.txt', "Title: " + movie.Title + "\n" + "Year: " + movie.Year + "\n" + "IMDB Rating: " + movie.imdbRating + "\n" + "Country: " + movie.Country + "\n" + "Language: " + movie.Language + "\n" +
-//         "Plot: " + movie.Plot + "\n" + "Actors: " + movie.Actors + "\n" + "Rotten Tomatoes Rating: " + movie.Ratings[0].Value);
-//       //
-//       //
-//     }
-//   });
-// }
-//
-// getMovie();
+var getMovie = function() {
+
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + argTwo + "&y=&plot=short&apikey=40e9cece";
+
+
+
+  request(queryUrl, function(error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      //parses response on console
+      var movie = JSON.parse(body);
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Release Year: " + JSON.parse(body).Year);
+      console.log("Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[0].Value);
+      console.log("Country of Production: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+      fs.appendFile('log.txt', "Title: " + movie.Title + "\n" + "Year: " + movie.Year + "\n" + "IMDB Rating: " + movie.imdbRating + "\n" + "Country: " + movie.Country + "\n" + "Language: " + movie.Language + "\n" +
+        "Plot: " + movie.Plot + "\n" + "Actors: " + movie.Actors + "\n" + "Rotten Tomatoes Rating: " + movie.Ratings[0].Value);
+      //
+      //
+    }
+  });
+}
+
+getMovie();
 
 var doWhatItSays = function() {
   fs.readFile("random.txt", "utf8", function(error, data) {
@@ -123,17 +121,16 @@ var doWhatItSays = function() {
     var dataArr = data.split(',')
 
     if (dataArr.length == 2) {
-      pick(dataArr[0], dataArr[1]);
+      choice(dataArr[0], dataArr[1]);
     } else if (dataArr.length == 1) {
-      pick(dataArr[0]);
+      choice(dataArr[0]);
     }
   });
 }
-
-
+doWhatItSays();
 
 //switch statement to run different apps
-var choice = function(action){
+var choice = function(){
 switch (argOne) {
     case 'my-tweets':
         getTweets();
